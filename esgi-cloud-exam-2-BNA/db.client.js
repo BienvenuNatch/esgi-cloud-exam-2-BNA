42217c84f058816ed2f7bc9f05ba9ede9d4ca99e
Bienvenu Natchia <b.natchia19@gmail.com>
2025-03-04 03:08:53 +0100

    connexion à une base de données postgre

--- esgi-cloud-exam-2-BNA/db.client.js
+++ esgi-cloud-exam-2-BNA/db.client.js
@@ -1,9 +1,7 @@
 const { Sequelize } = require('sequelize')
 
 // database
-const sequelize = new Sequelize(
-  'postgres://fakeurl', // TODO
-  {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialectOptions: {
       ssl: {
         require: true,
